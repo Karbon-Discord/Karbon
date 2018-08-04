01ba35609a9e96401370ca4bcb9c3fb90d9ef0a2
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-04 18:22:31 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,14 @@ const config = require('./botconfig.json');
 const bot = new Discord.Client({disableEveryone:true});
 bot.on("message", (message) => {
     let prefix = config.prefix;
+    let messageArray = message.content.split(" ");
+    let cmd = messageArray[0];
+    let args = messageArray.slice(1);
+    
+    if(cmd === `${prefix}hello`){
+        message.channel.send("Hey there!")
+    }
       
 })
 
-bot.login(config.token);
+bot.login(process.env.token);
