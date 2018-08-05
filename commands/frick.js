91004c225658e2fea73b5fbd7853a38615f40219
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-05 15:16:58 -0400

    Create frick.js

---
+++ commands/frick.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+   message.channel.send("my T O E")
+};
+
+module.exports.help = {
+  name: "frick"
+}
