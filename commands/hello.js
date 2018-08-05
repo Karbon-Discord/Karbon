3cbd4feba1a88a36676fc4f7534140d5f9cc0416
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-05 10:46:03 -0400

    Update hello.js

--- commands/hello.js
+++ commands/hello.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 
-module.exports.run(bot, message, args) => {
+module.exports.run = async (bot, message, args) => {
    message.channel.send("Hey there!")
 };
 
