090a215512a26a0f8909a4380e8d630247864504
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-05 10:43:44 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,16 +2,33 @@ const Discord = require("discord.js");
 const config = require('./botconfig.json');
 const token = process.env.token;
 const bot = new Discord.Client({disableEveryone:true});
+const fs = require("fs");
+bot.commands = new Discord.Collection();
+
+fs.readdir('./commands', (err, files) => {
+    let jsfile = files.filter(f => f.split(".").pop() === "js");
+    if(jsfile.length <= 0) console.log("Couldn't find the command!");
+
+    jsfile.forEach((f, i) => {
+        let props = require(`./commands/${f}`);
+        console.log(`${f} loaded!`);
+        bot.commands.set(props.help.name, props);
+
+    })
+
+})
+
 bot.on("message", (message) => {
     let prefix = config.prefix;
     let messageArray = message.content.split(" ");
     let cmd = messageArray[0];
     let args = messageArray.slice(1);
     
-    if(cmd === `${prefix}hello`){
-        message.channel.send("Hey there!")
-    }
-      
+     
+    let commandfile = bot.commands.get(cmd.slice(prefix.length));
+    if(commandfile) commandfile.run(bot, message, args);
+    
+ 
 })
 
 bot.login(process.env.token);
