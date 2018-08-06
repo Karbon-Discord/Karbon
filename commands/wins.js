c2d69b40e0d654a0f741c19fcaaed25bf6e6f6a1
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 16:33:55 -0400

    Update wins.js

--- commands/wins.js
+++ commands/wins.js
@@ -2,9 +2,9 @@ const Discord = require('discord.js');
 const wins = require('../wins.json');
 
 module.exports.run = async (bot, message, args) => {
-  var wins = wins[message.author.id].wins;
-  message.reply(`You have ${wins} wins.`)
-  console.log(wins)
+  var win = wins[message.author.id].wins;
+  message.reply(`You have ${win} wins.`)
+
 };
 
 module.exports.help = {
