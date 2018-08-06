c43f08bf2f2545d6a390a7d6b3a3d56167ae01a5
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 16:11:43 -0400

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -21,7 +21,7 @@ module.exports.run = async (bot, message, args) => {
     if(!user) message.reply("Please specify a user.")
     var winnerHealth = Math.floor(Math.random() * 99) + 1
     var loserHealth = 0;
-    var arr = [`${message.author.username}`, `${user}`];
+    var arr = [`${message.author.username}`, `${user.username}`];
     var randomwinner = Math.floor(Math.random() * arr.length);
         var winner = new Discord.RichEmbed()
         .setColor("#000000")
