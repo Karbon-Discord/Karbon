746c055491af214f23ae0301c4558c4f3cb7414d
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 16:17:22 -0400

    Create wins.js

---
+++ commands/wins.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+const wins = require('../wins.json');
+
+module.exports.run = async (bot, message, args) => {
+  var wins = wins[message.author.id].wins;
+  message.reply(`You have ${wins} wins.`)
+};
+
+module.exports.help = {
+  name: "wins"
+}
