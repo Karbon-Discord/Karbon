db6093f1f6adf828b92cdc29c2f2edb0256bd144
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 15:29:48 -0400

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -4,32 +4,40 @@ const losses = require('../losses.json');
 const xp = require('../xp.json');
 
 module.exports.run = async (bot, message, args) => {
+    if(!wins[message.author.id]){
+        wins[message.author.id] = {
+            wins:0
+        }
+    }
+    
+    if(!losses[message.author.id]){
+        losses[message.author.id] = {
+            losses:0
+        }
+    }
+    
+        if(!xp[message.author.id]){
+        xp[message.author.id] = {
+            xp:0,
+            level:1
+        }
+    }
+    
+    
     let user = message.mentions.users.first();
     if(user) return message.channel.send(`${user}, ${message.author.username} has challenged you to fight. Would you like to accept the challenge?`)
     else return message.reply("Please specify a user.")
-    
-    var res = ["yes", "no"];
-    message.channel.awaitMessages(response => response.content == 'yes' && message.author.id == user
-|| response.content == 'no' && message.author.id == user, 
-  {
-    max: 1,
-    time: 60000,
-    errors: ['time'],
-})
-
- .then((collected) => {
-                if (collected.first().content == 'yes') {
-                    message.channel.send(`${user} has accepted the challenge!`);
-                }
-                else if(collected.first().content == 'no') {
-                    message.channel.send(`Nope, challenge rejected.`);
-                }
-            })
-            .catch(() => {
-                message.channel.send(`No response. Fight has been cancelled.`);
-            });
-          
-};
+    var winnerHealth = Math.floor(Math.random * 99) + 1
+    var loserHealth = 0;
+    var arr = [`${message.author.id}`, `${user}`];
+    var randomwinner = Math.floor(Math.random() * arr.length);
+        var winner = new Discord.RichEmbed()
+        .setColor("#000000")
+        .setTitle(":crossed_swords: BATTLE [BETA]")
+        .addField(`${randomwinner} has won the battle!`)
+        .addField(`${winnerHealth} - 0`)
+    message.channel.send()
+   
 
 module.exports.help = {
   name: "fight"
