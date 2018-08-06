1b83c56a664720637468739e9f8caa1b85a67b69
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 16:39:29 -0400

    Update win.js

--- commands/win.js
+++ commands/win.js
@@ -1,11 +1,21 @@
-const Discord = require('discord.js');
-const wins = require('../wins.json');
+const Discord = require("discord.js");
+let wins = require("../wins.json");
 
 module.exports.run = async (bot, message, args) => {
-  var win12 = wins[message.author.id].wins;
-  message.channel.send(`You have ${win12} wins.`)
 
-};
+  if(!wins[message.author.id]){
+   wins[message.author.id] = {
+     wins: 0
+  };
+}
+  let curxp = wins[message.author.id].wins;
+  let lvlEmbed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setColor("#000000")
+  .addField("Wins", curxp, true);
+  message.channel.send(lvlEmbed).then(msg => {msg.delete(5000)});
+
+}
 
 module.exports.help = {
   name: "win"
