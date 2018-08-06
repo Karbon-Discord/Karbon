e09eeb0c764d13057cf9b97d8db4f7c5b3ef6d5d
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 13:20:30 -0400

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -5,16 +5,16 @@ const xp = require('../xp.json');
 
 module.exports.run = async (bot, message, args) => {
     let user = message.mentions.users.first();
-    if(user) return message.channel.send(`@${user}, ${message.author.username} has challenged you to fight. Would you like to accept the challenge?`)
+    if(user) return message.channel.send(`${user}, ${message.author.username} has challenged you to fight. Would you like to accept the challenge?`)
     .then(() => {
-        message.channel.awaitMessages(response => response.content == "yes" && message.author.id == user || response.content == "no" && message.author.id == user, {
+        message.channel.awaitMessages(response => response.content == "yes" && response.author.id == user || response.content == "no" && response.author.id == user, {
             max:1,
             time:40000,
             errors: ['time'],
         })
            .then((collected) => {
                 if (collected.first().content == 'yes') {
-                    message.channel.send(`${user} has accepted the challenge!`);
+                    message.channel.send(`:crossed_swords: ${user} has accepted the challenge!`);
                 }
                 else if(collected.first().content == 'no') {
                     message.channel.send(`Alright, so ${user} is scared. But oh well, challenge declined!`);
