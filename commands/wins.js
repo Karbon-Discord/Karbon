8eaa7814c92abd4895b00e998953bb50bfc9c4c7
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-06 16:31:17 -0400

    Update wins.js

--- commands/wins.js
+++ commands/wins.js
@@ -4,6 +4,7 @@ const wins = require('../wins.json');
 module.exports.run = async (bot, message, args) => {
   var wins = wins[message.author.id].wins;
   message.reply(`You have ${wins} wins.`)
+  console.log(wins)
 };
 
 module.exports.help = {
