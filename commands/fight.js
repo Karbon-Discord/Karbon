9d5cdf1e473556250acfb0035995205f6d6dbc5e
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-07 17:03:56 -0400

    Update fight.js

--- commands/fight.js
+++ commands/fight.js
@@ -18,10 +18,11 @@ module.exports.run = async (bot, message, args) => {
     
     
     let user = args[0];
+    let lol = args[1] 
     if(!user) message.reply("Please specify a user.")
     var winnerHealth = Math.floor(Math.random() * 99) + 1
     var loserHealth = 0;
-   var arr = [`${message.author.username}`, `${user.username}`];
+   var arr = [`${lol}`, `${user}`];
    var randomwinner = Math.floor(Math.random() * arr.length);
         var winner = new Discord.RichEmbed()
         .setColor("#000000")
@@ -29,14 +30,16 @@ module.exports.run = async (bot, message, args) => {
         .addField(`${arr[randomwinner]} has won the battle!`)
         .addField(`${winnerHealth} - 0`)
     message.channel.send(winner)
-    wins[message.author.id].wins = wins[message.author.id].wins += 1
+/*    wins[message.author.id].wins = wins[message.author.id].wins += 1
     fs.writeFile("../wins.json", JSON.stringify(wins), err => {
      if(err) console.log(err)
   })
+    
     xp[message.author.id].xp = xp[message.author.id].xp += 1;
         fs.writeFile("../xp.json", JSON.stringify(xp), err => {
         if(err) console.log(err)
-        })
+
+     }) */
 }
 
 module.exports.help = {
