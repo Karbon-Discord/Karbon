2529e09aa7ec0317af6413a7d82b6a44351d68ca
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-07 14:01:09 -0400

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+   let help = new Discord.RichEmbed()
+   .setTitle("***EPIC KARBON COMMANDS***")
+   .setColor("#bb00ff")
+   .addField("--help", "Shows a list of commands", true)
+   .addField("--dice [number]", "Roll a die :game_die:", true)
+   .addField("--fight [user_in_discord]", "DEATH BATTLE :eyes: :crossed_swords:", true)
+   .addField("--fortnite [username] [pc | psn | xbl ]", "My favorite food truck :fork_and_knife:", true)
+   .addField("--win", "Shows the number of wins you got from fighting :trophy:", true)
+   .addField("--frick", "ow", true)
+};
+
+module.exports.help = {
+  name: "help"
+}
