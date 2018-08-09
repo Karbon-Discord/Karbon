fbb24a43328cbf9f780f4abf1a97d499d66fbcc9
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-09 17:55:55 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -14,9 +14,9 @@ module.exports.run = async (bot, message, args) => {
         var followers = $('.stat').first().next().text();
         var updates = $('.stat').first().next().next().text();
         var tips = $('.stat').first().next().next().next().text();
-        var lastUpdated = $('.stat').first().next().next().next().next().text();
-        var create = $('.stat').first().next().next().next().next().next().text();
-          console.log(lastUpdated, create)
+//        var lastUpdated = $('.stat').first().next().next().next().next().text();
+        var create = $('.stats').first().next().text();
+          console.log(create)
 //         let embed = new Discord.RichEmbed()
 //         .setTitle(`Neocities stats for ${user}`)
 //         .setColor("#42f4d7")
