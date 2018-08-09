4eb61f46d55923231dd98d52ef644096b88b8b7c
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-08 20:50:21 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -2,17 +2,20 @@ const Discord = require('discord.js');
 const Neocities = require('neocities');
 const request = require('request');
 const cheerio = require('cheerio');
-const oof = new Neocities() 
+const oof = new Neocities('elementz', ) 
 module.exports.run = async (bot, message, args) => {
     var user = args[0];
     request(`https://neocities.org/site/${user}`, (error, response, html) => {
         if(!error && response.statusCode === 200){
           const $ = cheerio.load(html);
-          const comment = $('.news-item')[0];
-          const output = comment.find('.content').text();
+          const comment = $('.news-item');
+          const output = comment.find('.content').next().text();
           console.log(output);
         }
     })
+    
+    
+    
     
 };
 
