4873c72463609fedbd8a4b9099d9cd87455e174e
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-09 09:55:56 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -11,6 +11,7 @@ module.exports.run = async (bot, message, args) => {
           const arrcmt = [];
           var stat = $('.stat');
           var output = stat.find('strong').text();
+          console.log(output)
       });
             
         }
