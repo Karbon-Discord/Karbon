afafab3eae00b677271bd6d5c56fa57fccaae17e
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-09 17:35:03 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -1,5 +1,4 @@
 const Discord = require('discord.js');
-const Neocities = require('neocities');
 const request = require('request');
 const cheerio = require('cheerio');
 const oof = new Neocities() 
@@ -11,9 +10,12 @@ module.exports.run = async (bot, message, args) => {
           const comment = $('.news-item');
           var output = comment.find('.content').first().text().replace(/\s\s+/g, "\n");
           //message.channel.send(output);
+            
           if(!output){
             output = "Looks like he/she doesn't have anything to say at the moment!"
           }
+            
+          
         var views = $('.stat').first().text();
         var followers = $('.stat').first().next().text();
         var updates = $('.stat').first().next().next().text();
@@ -21,15 +23,7 @@ module.exports.run = async (bot, message, args) => {
         var lastUpdated = $('.stat').first().next().next().next().next().text();
         var create = $('.stat').first().next().next().next().next().next().text();
             
-        }
-
-    })
-    
-   oof.info(user, resp => {
-    var hits = resp.info.hits;
-   })
-        
-        let embed = new Discord.RichEmbed()
+              let embed = new Discord.RichEmbed()
         .setTitle(`Neocities stats for ${user}`)
         .setAuthor(`${output}`)
         .setColor("#42f4d7")
@@ -39,7 +33,13 @@ module.exports.run = async (bot, message, args) => {
         .addField(":money: Tips", tips, true)
         .addField(":tools: Last Updated", lastUpdated, true)
         .addField(":new_moon_with_face: Created", create, true)
-        .addField(":ballot_box_with_check: Hits", hits, true)
+            
+        }
+
+    })
+    
+
+      
     
     
 };
