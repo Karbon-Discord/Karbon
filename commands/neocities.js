5c708197c36711608126bc1166933a02f67ca192
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-09 17:46:12 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -7,12 +7,7 @@ module.exports.run = async (bot, message, args) => {
         if(!error && response.statusCode === 200){
           const $ = cheerio.load(html);
           const comment = $('.news-item');
-          var output = comment.find('.content').first().text();
-          message.channel.send(output);
-            
-          if(!output){
-            output = "Looks like he/she doesn't have anything to say at the moment!"
-          }
+
             
           
         var views = $('.stat').first().text();
@@ -21,18 +16,16 @@ module.exports.run = async (bot, message, args) => {
         var tips = $('.stat').first().next().next().next().text();
         var lastUpdated = $('.stat').first().next().next().next().next().text();
         var create = $('.stat').first().next().next().next().next().next().text();
-            console.log(output)
-//               let embed = new Discord.RichEmbed()
-//         .setTitle(`Neocities stats for ${user}`)
-//         .setAuthor(`${output}`)
-//         .setColor("#42f4d7")
-//         .addField(":eyes: Views", views, true)
-//         .addField(":dolphin: Followers", followers, true)
-//         .addField(":construction: Updates", updates, true)
-//         .addField(":money: Tips", tips, true)
-//         .addField(":tools: Last Updated", lastUpdated, true)
-//         .addField(":new_moon_with_face: Created", create, true);
-//             message.channel.send(embed)
+              let embed = new Discord.RichEmbed()
+        .setTitle(`Neocities stats for ${user}`)
+        .setColor("#42f4d7")
+        .addField(":eyes: Views", views, true)
+        .addField(":dolphin: Followers", followers, true)
+        .addField(":construction: Updates", updates, true)
+        .addField(":money: Tips", tips, true)
+        .addField(":tools: Last Updated", lastUpdated, true)
+        .addField(":new_moon_with_face: Created", create, true);
+            message.channel.send(embed)
             
         }
 
