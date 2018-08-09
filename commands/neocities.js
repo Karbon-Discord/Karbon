059f834ede19cfdea34e03a97aca44c1b682b6dd
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-08 20:58:24 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -8,8 +8,8 @@ module.exports.run = async (bot, message, args) => {
     request(`https://neocities.org/site/${user}`, (error, response, html) => {
         if(!error && response.statusCode === 200){
           const $ = cheerio.load(html);
-          const comment = $('.news-item')[0];
-          const output = comment.find('.content').text();
+          const comment = $('.news-item');
+          const output = comment.find('.content').get(0).text();
           console.log(output)
         }
     })
