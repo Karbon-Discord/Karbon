445a71e0ef3230a3636eaca592d8f5f7a88996b1
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-10 09:20:15 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -3,6 +3,7 @@ const request = require('request');
 const cheerio = require('cheerio');
 module.exports.run = async (bot, message, args) => {
     var user = args[0];
+    var views, follow, update, tip, lastupdate, create;
     request(`https://neocities.org/site/${user}`, (error, response, html) => {
         if(!error && response.statusCode === 200){
           const $ = cheerio.load(html);
@@ -12,14 +13,15 @@ module.exports.run = async (bot, message, args) => {
 
           $('.stat strong').each(function( index ) {
               arr.push($(this).text().trim());
-              var views = arr[0];
-              var follow = arr[1]
-              var update = arr[2]
-              var tip = arr[3]
-              var lastupdate = arr[4]
-              var create = arr[5]
-              
-              let embed = new Discord.RichEmbed()
+              views = arr[0];
+              follow = arr[1]
+              update = arr[2]
+              tip = arr[3]
+              lastupdate = arr[4]
+              create = arr[5]
+            
+      });
+ let embed = new Discord.RichEmbed()
         .setTitle(`Neocities stats for ${user}`)
         .setColor("#42f4d7")
         .addField(":eyes: Views", user, true)
@@ -29,9 +31,6 @@ module.exports.run = async (bot, message, args) => {
         .addField(":tools: Last Updated", lastupdate, true)
         .addField(":new_moon_with_face: Created", create, true);
          message.channel.send(embed)
-            
-      });
-
         
         }
 
