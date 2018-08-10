3571d900928753d6473547ec89ed65a7766286ac
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-09 21:33:54 -0400

    Update neocities.js

--- commands/neocities.js
+++ commands/neocities.js
@@ -16,7 +16,7 @@ module.exports.run = async (bot, message, args) => {
         var updates = $('.stat').first().next().next().text();
         var tips = $('.stat').first().next().next().next().text();
         var lastdate = $('.stat').first().next().next().next().next().text();
-        var createww = $('.stat').first().next().next().next().next().next().text();
+        var create = $('.stat').first().next().next().next().next().next().text();
             console.log(create)
  //         console.log(create)
 //         let embed = new Discord.RichEmbed()
