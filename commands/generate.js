e68dc52c6af238b825c60b29a9f74130d225eebb
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-10 12:52:17 -0400

    Create generate.js

---
+++ commands/generate.js
@@ -0,0 +1,18 @@
+const Discord = require("discord.js");
+let ticket = require("../lotteryticket.json");
+
+module.exports.run = async (bot, message, args) => {
+var id = Math.floor(Math.random() * 4000000) + 1000000
+
+if(!ticket[message.author.id]){
+   ticket[message.author.id] = {
+     ticketid: id
+  };
+}
+ message.reply(`**Your Lottery ID is: ${id}. Please remember to not run this command.`);
+
+}
+
+module.exports.help = {
+  name: "generate"
+}
