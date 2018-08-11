f0c21a3d472cf43566705f8a50051972fd6b5fe2
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-11 10:32:07 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -2,14 +2,12 @@ const Discord = require("discord.js");
 let ticket = require("../lotteryticket.json");
 
 module.exports.run = async (bot, message, args) => {
-  
-  var id = Math.floor(Math.random() * 4000000) + 1000000
-  if(!ticket[message.author.id]){
-  message.reply("Please run the command: --generate.")
-}
-  else{
+//   if(!ticket[message.author.id]){
+//   message.reply("Please run the command: --generate.")
+// }
+//   else{
     message.channel.send(`Okay, so these are the tickets that got generated: ${ticket.ticket}`)
-  }
+//  }
   
 // message.channel.send(ticket);
   
