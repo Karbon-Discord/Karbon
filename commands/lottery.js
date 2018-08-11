d7e0c2a68751051505f6d82fedc70cef7878664b
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-11 10:24:31 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -7,9 +7,11 @@ module.exports.run = async (bot, message, args) => {
   if(!ticket[message.author.id]){
   message.reply("Please run the command: --generate.")
 }
+  else{
+    message.channel.send(`Okay, so these are the tickets that got generated: ${ticket.ticket}`)
+  }
   
-  
- message.channel.send(ticket);
+// message.channel.send(ticket);
   
 
 }
