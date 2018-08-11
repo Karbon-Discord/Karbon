a5b6bb5da4fce3e25ebf72b4f7ad95da6ee42c89
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-11 12:34:17 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
 if(!ticket[message.author.username]){
    var id = Math.floor(Math.random() * 4000000) + 1000000
 
-   ticket[message.author.username] = {
+   ticket = {
      ticketid: id
   };
    
