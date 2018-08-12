16f25872f13ec1dcd4ff83fd27480f179fcb1d24
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-12 14:01:52 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 let ticket = require("../lotteryticket.json");
 
 module.exports.run = async (bot, message, args) => {
-   if(!ticket[message.author.username].username){
+   if(!ticket.ticketid){
    message.reply("Please run the command: --generate.")
  }
    else{
