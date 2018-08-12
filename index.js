0583b36a8091e4b3eba8abebd8874b81847e8231
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-12 19:35:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -23,12 +23,12 @@ bot.on('ready', () => {
 bot.on('guildMemberAdd', member => {
     bot.channels.find("id", "478309556330430464").sendMessage(`**Welcome aboard, ${member.user}! Please welcome him/her! If you have any questions, feel free to DM one of the Developers or me!** `);
     let embed = new Discord.RichEmbed()
-      .addField(`:inbox_tray: **${member.user.username}** has joined the server!`, ` `)
+      .setDescription(`:inbox_tray: **${member.user.username}** has joined the server!`)
       .setFooter("User Joined | Karbon's Lounge", member.user.displayAvatarURL)
        
      .setColor("#42f47d")
   
-    .setThumbnail(`${member.user.displayAvatarURL}`);
+    .setThumbnail(`${member.guild.iconURL}`);
     bot.channels.find("id", "478309556330430464").sendMessage(embed);
 });
 
