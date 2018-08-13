0b592e2f31c5b9f704253d56513beef48e54e67b
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-13 09:33:19 -0400

    Update kick.js

--- commands/kick.js
+++ commands/kick.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    
+   message.react(":x:")
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
     if(!kUser) return message.channel.send("Can't find user!");
     let kReason = args.join(" ").slice(22);
