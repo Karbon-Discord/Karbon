ec5ff8ee75fd890038bc9b5b093122cf879e90d1
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-14 14:59:39 -0400

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -5,20 +5,21 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
   const agree = "✅"
 const disagree = "❌"
-  const question = args.join(' ');
-  //if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.channel.send("no")
+  const question = args.join(" ");
   if(!question){
   message.react('🤔');
     message.channel.send("**CORRECT USAGE: --vote [question] **");
   }
   else{
-  let msg = await message.guild.channels.find(`name`, 'vote').send(`<@everyone> **VOTING QUESTION: ${question}? **`)
-        await msg.react(agree);
-        await msg.react(disagree);
-          const reactions = await msg.awaitReactions(reaction => reaction.emoji.name == agree || reaction.emoji.name == disagree, {time: 2000});
-          message.channel.send(`Voting complete! We got ${reactions.get(agree).count-1} as the first option, and ${reactions.get(disagree).count-1} as the second option!`);
-     
-  }
+  let msg = await message.guild.channels.find(`name`, 'vote').send(`@everyone **VOTING QUESTION: ${question}? You got ${time} seconds!`)
+      message.react(agree);
+      message.react(disagree)
+         await msg.react(agree);
+        await msg.react(disagree)
+        
+    
+    
+    
 
 }
 
