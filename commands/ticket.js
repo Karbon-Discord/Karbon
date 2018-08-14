48a26a50844fa4eb9e4a3dceb6421171f0770330
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-14 19:38:17 -0400

    Update ticket.js

--- commands/ticket.js
+++ commands/ticket.js
@@ -2,18 +2,6 @@ const Discord = require("discord.js");
 let ticket = require("../lotteryticket.json");
 
 module.exports.run = async (bot, message, args) => {
-if(!ticket[message.author.id]){
-  
-   message.reply("You do not have a ticket! Feel free to generate one.");
-
-}
-   else{
-     var ticket1 = ticket[message.author.id].ticketid;
-    message.reply(`Your ticket is: ${ticket1}.`);
-    
-    
-  }
-
 }
 
 module.exports.help = {
