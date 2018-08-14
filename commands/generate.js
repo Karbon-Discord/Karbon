a695a81f9c389fa121e8259939b93342f8d3c585
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-14 19:59:02 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -13,7 +13,7 @@ if(!ticket.tickets){
 
    var id = Math.floor(Math.random() * 4000000) + 1000000
    var oof = ticket.tickets;
-   oof.push(`${id}`);
+   oof.push(`{"id": ${message.author.id}, "ticket": ${id}}`);
    console.log(oof);
    fs.writeFile('../lotteryticket.json', JSON.stringify(id), err => {
          if(err) console.log(err);
