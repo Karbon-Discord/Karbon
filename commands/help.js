e8d15f5f2b80269536f7922d0fc9724250369c90
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-14 19:20:35 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,7 +1,9 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-   message.author.send("oof")
+   message.author.send(`
+   COMING SOON
+`)
 };
 
 module.exports.help = {
