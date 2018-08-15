ad35bba5d99399d0027555bba777c48d57d7e74a
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-15 19:39:14 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -3,12 +3,18 @@ let ticket = require("../lotteryticket.json");
 const fs = require('fs');
 let oof = require('../storage.json');
 module.exports.run = async (bot, message, args) => {
+      var id = Math.floor(Math.random() * 4000000) + 1000000
 
 if(!ticket[message.author.id]){
-      var id = Math.floor(Math.random() * 4000000) + 1000000
    ticket[message.author.id] = {
       ticketid: []
    } 
+}    
+    if(!oof[message.author.id]){
+   oof[message.author.id] = {
+      id2: id
+   } 
+    }    
       if(message.author.id === "357307569397694466"){
       var oof = ticket[message.author.id].ticketid;
       oof.push(id);
