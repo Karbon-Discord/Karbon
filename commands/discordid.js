5f2309ea1289b3fa43d27d98d36827aec468cda5
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-15 14:48:19 -0400

    Create discordid.js

---
+++ commands/discordid.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+    message.react("🆔");
+    message.reply(`**Your Discord ID is: ${message.channel.id}`)
+}
+
+module.exports.help = {
+  name: "discordid"
+}
