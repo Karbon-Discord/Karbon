0985b39c8eabc76f4593e09b76bde2cc483a9f67
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-16 12:48:38 -0400

    Update generate.js

--- commands/generate.js
+++ commands/generate.js
@@ -1,16 +1,13 @@
 const Discord = require("discord.js");
 let ticket = require("../lotteryticket.json");
 const fs = require('fs');
-let oof2 = require('../storage.json');
 module.exports.run = async (bot, message, args) => {
-      var id = Math.floor(Math.random() * 4000000) + 1000000
 
 if(!ticket[message.author.id]){
+      var id = Math.floor(Math.random() * 4000000) + 1000000
    ticket[message.author.id] = {
       ticketid: []
    } 
-
-
       if(message.author.id === "357307569397694466"){
       var oof = ticket[message.author.id].ticketid;
       oof.push(id);
@@ -20,14 +17,13 @@ if(!ticket[message.author.id]){
 }
       else {
 
-var oof = ticket['357307569397694466'].ticketid;
+var oof = ticket[357307569397694466].ticketid;
             oof.push(id)
             console.log(oof);
       };
 fs.writeFile('../lotteryticket.json', JSON.stringify(id), err => {
          if(err) console.log(err);
    })
-     
    
 //    var oof = ticket.tickets;
 //    oof.push('["id": ${message.author.id}, "ticket": ${id}]');
@@ -36,7 +32,6 @@ fs.writeFile('../lotteryticket.json', JSON.stringify(id), err => {
  
     message.reply(`**Your ticket number is: ${id}. **`);
 
-}
 }
    else{
     message.reply("You have already generated a ticket!");
