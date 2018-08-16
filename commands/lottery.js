9765a0a2d3211c6ab65c30d4d3e1c3402a4826f0
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-16 18:55:38 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -2,12 +2,13 @@ const Discord = require("discord.js");
 let ticket = require("../lotteryticket.json");
 let storage = require("../storage.json");
 module.exports.run = async (bot, message, args) => {
- let lol = Object.keys(storage).length;
+ let lol = Object.keys(storage);
+ let randomness = Math.floor(Math.random() * lol.length)
  console.log(lol)
 let msg = await message.channel.send(":warning: | **CONFIRMATION: Are you sure you want to run the lottery and pick the winners?**");
  const res = await message.channel.awaitMessages(response => {
   response.content === "yes" || response.content === "no"
-  if(response.content === "yes") message.reply(`Okay, so these are the winners of the lottery: <@${message.author.id}`)
+  if(response.content === "yes") message.reply(`Okay, so these are the winners of the lottery: <@${lol[randomness]}>, <@${lol[randomness]}>`)
 
  })
 };
