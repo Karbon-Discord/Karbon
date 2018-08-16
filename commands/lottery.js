4427d882ad7404d7b29047c2552b614001000aea
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-16 16:39:27 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -4,7 +4,7 @@ let ticket = require("../lotteryticket.json");
 module.exports.run = async (bot, message, args) => {
 let msg = await message.channel.send(":warning: | **CONFIRMATION: Are you sure you want to run the lottery and pick the winners?**");
  const res = await msg.channel.awaitMessages(response => response.content === "yes" || response.content === "no", {time: 40000});
- if(response.content === "yes") console.log("yes");
+ if(response.content === "yes") message.channel.send("yes");
 };
 
 module.exports.help = {
