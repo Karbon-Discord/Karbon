5d3a118f7888b48d0a1c3400973a433d90418c2a
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-17 17:01:59 -0400

    Update katoms.js

--- commands/katoms.js
+++ commands/katoms.js
@@ -4,11 +4,13 @@ let katoms = require("../katoms.json");
 module.exports.run = async (bot, message, args) => {
   if(!katoms[message.author.id]) {
     message.react("🤔"); 
-    message.channel.send(`You have ${katoms[message.author.id].katoms} Katoms!`)
     katoms[message.author.id] = {
        katoms:100
     }
+        message.channel.send(`You have ${katoms[message.author.id].katoms} Katoms!`)
+
   }
+  
   
   
   else {
