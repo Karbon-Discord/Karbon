d009a61923fb2f98d7603aef768673188e610298
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-17 10:44:23 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -20,12 +20,16 @@ module.exports.run = async (bot, message, args) => {
 let msg = await message.channel.send(":warning: | **CONFIRMATION: Are you sure you want to run the lottery and pick the winners?**");
  const res = await message.channel.awaitMessages(response => {
   response.content === "yes" || response.content === "no"
-  if(response.content === "yes") message.reply(`Okay, so these are the winners of the lottery: <@${lol[randomness]}>, who earned 250 Katoms <@${lol[randomness2]}>, who earned 500 Katoms, as well as <@lol[randomness3>, who earned 750 Katoms + A special role!`)
-  katoms[lol[randomness]].katoms += 250;
-  katoms[lol[randomness2]].katoms += 500;
+  if(response.content === "yes"){
+   message.reply(`Okay, so these are the winners of the lottery: <@${lol[randomness]}>, who earned 250 Katoms <@${lol[randomness2]}>, who earned 500 Katoms, as well as <@lol[randomness3>, who earned 750 Katoms + A special role!`)
+
+  katoms[`${lol[randomness]}`].katoms += 250;
+  katoms[`${lol[randomness2]}`].katoms += 500;
 //  katoms[lol[randomness3]].katoms += 750;
   console.log(katoms);
 
+  }
+  else if(response.content === "no") return message.reply(":x: | **Lottery Canceled**")
  })
 };
 
