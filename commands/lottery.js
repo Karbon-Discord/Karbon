dfd38d1446d2e7bea647b80e171b5eda19dbe5eb
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-17 15:26:16 -0400

    Update lottery.js

--- commands/lottery.js
+++ commands/lottery.js
@@ -4,7 +4,9 @@ let katoms = require("../katoms.json");
 const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
  
-
+var num1 = Math.floor(Math.random() * 100) + 150;
+ var num2 = Math.floor(Math.random() * 200) + 300;
+ var num3 = Math.floor(Math.random() * 500) + 300;
 
  var lol = Object.keys(ticket)
  console.log(lol)
@@ -15,11 +17,19 @@ let msg = await message.channel.send(":warning: | **CONFIRMATION: Are you sure y
  //  message.reply(`Okay, so these are the winners of the lottery: <@${lol[randomness]}>, who earned 250 Katoms <@${lol[randomness2]}>, who earned 500 Katoms, as well as <@lol[randomness3>, who earned 750 Katoms + A special role!`)
 
  var thirdplace = Math.random() * lol.length;
-//  var secondplace = Math.random() * lol.length;
-//  var firstplace = Math.random() * lol.length;
-   
-// katoms[thirdplace].katoms = katoms[`${thirdplace}`].katoms += 250;
-console.log(katoms[`357307569397694466`]);
+  var secondplace = Math.random() * lol.length;
+  var firstplace = Math.random() * lol.length;
+if(!katoms[thirdplace]){
+ katoms[thirdplace] = {
+  katoms:100
+ }
+  katoms[thirdplace].katoms = katoms[`${thirdplace}`].katoms += num1;
+ fs.writeFile('../katoms.json', JSON.stringify(katoms), err => {
+   if(err) console.log(err);
+ })
+}   
+
+   else  katoms[thirdplace].katoms = katoms[`${thirdplace}`].katoms += num1;
   }
   else if(response.content === "no") return message.reply(":x: | **Lottery Canceled**")
  })
