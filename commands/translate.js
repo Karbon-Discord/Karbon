cd05fce1e07505b31754f70fc56861b8290275e9
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-17 21:00:05 -0400

    Update and rename define.js to translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,14 +1,15 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-const dictionary = process.env.dictionary;
+const translate = process.env.translate;
 module.exports.run = async (bot, message, args) => {
     console.log("Fetch ready!");
     message.react("📚");
     var word = args[0]
-    fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${dictionary}&lang=en-ru&text=${word}`)
+    var language = args[1]
+    fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${translate}&word=${word}&lang=${language}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.def[0].tr);
+        console.log(data);
     })
 }
 
