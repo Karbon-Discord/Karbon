1b4946000fcd5209873c8941737d419adac90a92
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-18 15:28:54 -0400

    Create weather.js

---
+++ commands/weather.js
@@ -0,0 +1,20 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+const key = process.env.weather;
+module.exports.run = async (bot, message, args) => {
+    console.log("Fetch ready!");
+    message.react("⛈");
+    var city = args[0]
+    var state = args[1]
+    if(!state){
+    fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}`)
+    .then(res => res.json())
+    .then(data => {
+    console.log(data);
+    })
+}
+}
+
+module.exports.help = {
+  name: "weather"
+}
