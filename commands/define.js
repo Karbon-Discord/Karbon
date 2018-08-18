b6b175488b2893977098f0b7f9ded9eda6e98e7f
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-17 20:12:50 -0400

    Update define.js

--- commands/define.js
+++ commands/define.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
-
+const dictionary = process.env.dictionary;
 module.exports.run = async (bot, message, args) => {
     console.log("Fetch ready!");
     message.react("📚");
     var word = args[0]
-    fetch(`https://wordsapiv1.p.mashape.com/words/${word}`)
+    fetch(`https://dictionary.yandex.net/api/v1/dicservice.json/lookup?key=${dictionary}&lang=en-ru&text=${word}`)
     .then(res => res.json())
     .then(data => {
         console.log(data);
