9fef9e3c5585d513f71d07719faef683b662d3fa
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-18 15:39:02 -0400

    Update weather.js

--- commands/weather.js
+++ commands/weather.js
@@ -6,13 +6,13 @@ module.exports.run = async (bot, message, args) => {
     message.react("⛈");
     var city = args.slice(1).join(" ")
     //var state = args[1]
-    if(!state){
+  //  if(!state){
     fetch(`https://api.weatherbit.io/v2.0/current?city=${city}&key=${key}`)
     .then(res => res.json())
     .then(data => {
     console.log(data);
     })
-}
+//}
 }
 
 module.exports.help = {
