8224b3b276e6823e41fed01eb3264a781aba836f
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-19 09:37:19 -0400

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,10 +1,11 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-/*
+
 const help = fs.readFileSync('commands/commands.txt', 'utf8');
-*/
+
 module.exports.run = async (bot, message, args) => {
-   message.author.send("Read the help file here: https://raw.githubusercontent.com/Karbon-Discord/Karbon/master/commands/commands.txt");
+ //  message.author.send("Read the help file here: https://raw.githubusercontent.com/Karbon-Discord/Karbon/master/commands/commands.txt");
+   message.author.send(help)
 };
 module.exports.help = {
   name: "help"
