503b92c7c1255bcc17c8f1b05d18b14c4626cc36
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-18 20:44:05 -0400

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -2,6 +2,10 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
    var diceVal = Math.floor(Math.random() * args[0]);
+   if(!args[0]){
+    diceVal = Math.floor(Math.random() * 6) + 1;
+   }
+    
    message.channel.send(`The dice value outputted ${diceVal}.`);
 };
 
