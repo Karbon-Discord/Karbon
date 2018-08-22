4cdd4bd46f2bb32559ab0711c353a87a4452ba6b
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 16:15:43 -0400

    Create transfer.js

---
+++ commands/transfer.js
@@ -0,0 +1,19 @@
+const Discord = require('discord.js');
+const katoms = require('../katoms.json');
+const bankacc = require('../katoms2.json');
+const errors = require('./utils/errors.js');
+
+var num = args[0];
+if(!bankacc[message.author.id]){
+  bankacc[message.author.id] = {
+    katoms:25
+  }
+}
+
+if(!num) return errors.correctUsage(message, "--transfer [num. katoms]");
+else if(!katoms[message.author.id]) return errors.noKatoms(message);
+else{
+  katoms[message.author.id].katoms = katoms[message.author.id].katoms -= num;
+  bankacc[message.author.id].bankacc = bankacc[message.author.id].bankacc += num;
+  message.reply("**Transfer Successful!**")
+}
