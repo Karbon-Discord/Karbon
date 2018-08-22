bfbf012132eee00d6d9f1092a5a94a275f88b8d9
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 16:17:59 -0400

    Update transfer.js

--- commands/transfer.js
+++ commands/transfer.js
@@ -3,6 +3,7 @@ const katoms = require('../katoms.json');
 const bankacc = require('../katoms2.json');
 const errors = require('./utils/errors.js');
 
+module.exports.run = async (bot, message, args) => {
 var num = args[0];
 if(!bankacc[message.author.id]){
   bankacc[message.author.id] = {
@@ -16,4 +17,8 @@ else{
   katoms[message.author.id].katoms = katoms[message.author.id].katoms -= num;
   bankacc[message.author.id].bankacc = bankacc[message.author.id].bankacc += num;
   message.reply("**Transfer Successful!**")
+}
+}
+module.exports.help = {
+  name: "transfer"
 }
