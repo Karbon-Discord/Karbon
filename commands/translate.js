c1dfdf3f7cf6e2a915ad04131900b214718ba9cb
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 17:50:55 -0400

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,11 +1,13 @@
 const Discord = require('discord.js');
 const fetch = require('node-fetch');
+const errors = require('./utils/errors.js')
 const translate = process.env.translate;
 module.exports.run = async (bot, message, args) => {
     console.log("Fetch ready!");
     message.react("📚");
     var language = args[0]
     var phrase = args.join(" ").slice(2)
+    if(!language || !phrase || !language && !phrase) return errors.correctUsage(message, "--translate [lang code] [phrase / word]")
     fetch(`https://translate.yandex.net/api/v1.5/tr.json/translate?key=${translate}&text=${phrase}&lang=en-${language}`)
     .then(res => res.json())
     .then(data => {
