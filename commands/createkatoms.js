fe6335f8c3821ea7b55c27bd1a55ba0b16203968
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 16:42:32 -0400

    Update createkatoms.js

--- commands/createkatoms.js
+++ commands/createkatoms.js
@@ -1,5 +1,6 @@
 const Discord = require("discord.js");
 let katoms = require("../katoms.json");
+const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
  
  
@@ -8,6 +9,9 @@ module.exports.run = async (bot, message, args) => {
     katoms[message.author.id] = {
       katoms:100
     }
+  fs.writeFile('../katoms.json', JSON.stringify(katoms), err => {
+   if(err) console.log(err);
+  })
  }
 };
 
