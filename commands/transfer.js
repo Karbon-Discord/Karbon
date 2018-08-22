b77e36c1629dedf114aeb8d70850bc91f401249d
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 16:43:12 -0400

    Update transfer.js

--- commands/transfer.js
+++ commands/transfer.js
@@ -2,20 +2,31 @@ const Discord = require('discord.js');
 const katoms = require('../katoms.json');
 const bankacc = require('../katoms2.json');
 const errors = require('./utils/errors.js');
-
+const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
 var num = args[0];
 if(!bankacc[message.author.id]){
   bankacc[message.author.id] = {
     katoms:25
   }
+  fs.writeFile('../bankacc.json', JSON.stringify(katoms), err => {
+    if(err) console.log(err);
+  })
 }
 
 if(!num) return errors.correctUsage(message, "--transfer [num. katoms]");
 else if(!katoms[message.author.id]) return errors.noKatoms(message);
 else{
   katoms[message.author.id].katoms -= num;
+  fs.writeFile('../katoms.json', JSON.stringify(katoms), err => {
+    if(err) console.log(err);
+  })
+  
   bankacc[message.author.id].katoms += num;
+  
+  fs.writeFile('../bankacc.json', JSON.stringify(katoms), err => {
+    if(err) console.log(err);
+  })
   message.reply(`**Transfer Successful! You now have: ${katoms[message.author.id].katoms} Katoms, and your alt bank account has ${bankacc[message.author.id].katoms} Katoms!**`)
 }
 }
