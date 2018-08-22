74224ca7413080e6453269723f65a64f81fe68d4
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 18:13:36 -0400

    Update errors.js

--- commands/utils/errors.js
+++ commands/utils/errors.js
@@ -1,11 +1,11 @@
 const Discord = require('discord.js');
 const fs = require('fs');
-
+const config = require('../../botconfig.json');
 module.exports.correctUsage = (message, usage) => {
   let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setTitle("**INCORRECT USAGE**")
-  .setColor("#f48c42")
+  .setColor(config.orange)
   .addField("Correct Usage", usage);
   message.channel.send(embed);
 }
@@ -14,7 +14,16 @@ module.exports.noKatoms = (message) => {
   let embed = new Discord.RichEmbed()
   .setAuthor(message.author.username)
   .setTitle("**YOU DO NOT HAVE A KATOMS DB**")
-  .setColor("#f48c42")
+  .setColor(config.orange)
   .addField("Run this command", "--createkatoms");
+  message.channel.send(embed);
+}
+
+module.exports.noPerms = (message, perm) => {
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setTitle("**NO PERMS**")
+  .setColor(config.red)
+  .addField("Insufficent Perm", "--createkatoms");
   message.channel.send(embed);
 }
