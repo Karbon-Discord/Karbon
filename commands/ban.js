d85cdef268b7a7ecf3d055275057f37c820ba510
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 20:37:15 -0400

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,11 +1,12 @@
 const Discord = require('discord.js');
-
+const errors = require('./utils/errors.js')
 module.exports.run = async (bot, message, args) => {
     message.react(":no_entry:")
   let kUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-    if(!kUser) return message.channel.send("Can't find user!");
+    if(!kUser) return errors.userNotFound(message);
     let kReason = args.join(" ").slice(22);
-    if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("**Sorry, but you cannot kick that person!**");
+    
+    if(!message.member.hasPermission("BAN_MEMBERS")) return errors.noPerms(message, "BAN_MEMBERS");
     if(kUser.hasPermission("BAN_MEMBERS")) return message.channel.send("**The user you requested to be kicked cannot be kicked!**");
 
    let embed = new Discord.RichEmbed()
