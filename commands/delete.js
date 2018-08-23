a702a9355b8a21682448b4874543df3879871c15
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 20:02:59 -0400

    Update delete.js

--- commands/delete.js
+++ commands/delete.js
@@ -1,10 +1,10 @@
 const Discord = require('discord.js');
-
+const errors = require('./utils/errors.js')
 module.exports.run = async (bot, message, args) => {
     
   let numMsg = args[0];
   if(!message.member.hasPermission("MANAGE_MESSAGES")) return message.reply("no.");
-  if(!numMsg) return message.reply("no.");
+  if(!numMsg) return errors.correctUsage(message, "--delete [num. messages]");
   message.channel.bulkDelete(numMsg).then(() => {
     message.channel.send(`Deleted ${numMsg} messages successfully!`).then(msg => msg.delete(3000))
   })
