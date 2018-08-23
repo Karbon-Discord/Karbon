68c1938ccb5d8fb8b4e0987bd776d445b8fcdca6
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-22 20:16:47 -0400

    Update yt.js

--- commands/yt.js
+++ commands/yt.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-
+const errors = require('./utils/errors.js');
 module.exports.run = async (bot, message, args) => {
     var search = require('youtube-search');
  
@@ -8,6 +8,7 @@ module.exports.run = async (bot, message, args) => {
       key: process.env.ytkey
     };
     let nm = args.join(' ');
+    if(!nm) return errors.correctUsage(message, "--yt [video title name]")
     search(nm, opts, function(err, results) {
       if(err) return console.log(err);
       message.channel.send(':movie_camera: ***' + results[0].title + '*** - ' + results[0].channelTitle + '\n' + results[0].link).catch(console.error);
