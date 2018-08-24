c99c8cd4d343ed66c34db10091bf3befb2cba10c
elementz <bfdifan21@gmail.com>
2018-08-24 12:56:16 -0400

    XP System

---
+++ commands/level.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const errors = require('./utils/errors.js');
+module.exports.run = async (bot, message, args) => {
+   
+
+}
+
+module.exports.help = {
+    name: "level"
+}
