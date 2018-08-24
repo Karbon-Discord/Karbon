555d57f55c152299f4304852031899c05f85e8a6
elementz <bfdifan21@gmail.com>
2018-08-24 15:44:39 -0400

    XP System

--- commands/rank.js
+++ commands/rank.js
@@ -3,7 +3,7 @@ const xp = require('../xp.json')
 const errors = require('./utils/errors.js')
 module.exports.run = async (bot, message, args) => {
  var oof = Object.keys(xp);
- 
+ console.log(oof)
     
 };
 
