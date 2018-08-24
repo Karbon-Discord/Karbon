346ae346e68069473c3a15c42ac60d8622e9709a
elementz <bfdifan21@gmail.com>
2018-08-24 16:22:38 -0400

    XP System

--- commands/rank.js
+++ commands/rank.js
@@ -2,9 +2,8 @@ const Discord = require('discord.js');
 const xp = require('../xp.json')
 const errors = require('./utils/errors.js')
 module.exports.run = async (bot, message, args) => {
- var oof = Object.keys(xp);
  var arr = [];
- arr.push(oof);
+ arr.push(xp);
  console.log(arr);
     
 };
