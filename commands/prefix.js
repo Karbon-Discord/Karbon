12fa9dc5e5987b1b64070bb6e07bc1b6241456d8
elementz <bfdifan21@gmail.com>
2018-08-25 09:57:50 -0400

    XP System

--- commands/prefix.js
+++ commands/prefix.js
@@ -1,6 +1,5 @@
 const Discord = require('discord.js');
 const errors = require('./utils/errors.js');
-const botconfig = require('../config.json')
 const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
     
