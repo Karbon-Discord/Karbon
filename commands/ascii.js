23a0ccd298a8295471fd3ad6a2b25657c53ee0d8
elementz <bfdifan21@gmail.com>
2018-08-25 19:19:15 -0400

    ASCII

---
+++ commands/ascii.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const fetch = require('node-fetch');
+module.exports.run = async (bot, message, args) => {
+  
+    fetch(`http://artii.herokuapp.com/make?text=${args.join(' ').slice(1)}`)
+    .then(res => res.text)
+    .then(data => {
+        message.channel.send(```
+        ${data}
+        ```)
+    })    
+
+}
+
+module.exports.help = {
+  name: "ascii"
+}
