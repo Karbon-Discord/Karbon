0e92ca0dff1ca70821bad7344625b932a0897927
elementz <bfdifan21@gmail.com>
2018-08-25 10:28:20 -0400

    XP System

--- commands/give.js
+++ commands/give.js
@@ -3,6 +3,7 @@ const katoms = require('../katoms.json');
 const errors = require('./utils/errors.js');
 const fs = require('fs');
 module.exports.run = async (bot, message, args) => {
+    if(!message.author.hasPermission("ADMINISTRATOR")) return message.reply("no");
     var num = args[0];
     var pers = message.guild.member(message.mentions.users.first()) || message.guild.members.get(args[0]);
     if(!katoms[message.author.id]){
@@ -17,8 +18,6 @@ module.exports.run = async (bot, message, args) => {
 
     let pCoins = katoms[pers.id].katoms;
     let uCoins = katoms[message.author.id].katoms;
-    if(uCoins < num) return message.reply(":x: | Not enough Katoms!**");
-    katoms[message.author.id].katoms = katoms[message.author.id].katoms - parseInt(num);
     katoms[pers.id].katoms = katoms[pers.id].katoms + parseInt(num);
 }
 module.exports.help = {
