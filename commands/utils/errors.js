75a16d98b44068e9a2d8e18ddd33f39e3d6310cb
elementz <bfdifan21@gmail.com>
2018-08-26 09:20:22 -0400

    oof

---
+++ commands/utils/errors.js
@@ -0,0 +1,42 @@
+const Discord = require('discord.js');
+const fs = require('fs');
+const config = require('../../botconfig.json');
+module.exports.correctUsage = (message, usage) => {
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setTitle("**INCORRECT USAGE**")
+  .setColor(config.orange)
+  .addField("Correct Usage", usage);
+  message.channel.send(embed);
+}
+
+module.exports.noKatoms = (message) => {
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setTitle("**YOU DO NOT HAVE A KATOMS DATABASE**")
+  .setColor(config.orange)
+  .addField("Run this command", "--createkatoms OR --katoms");
+  message.channel.send(embed);
+}
+
+module.exports.noPerms = (message, perm) => {
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setTitle("**NO PERMS**")
+  .setColor(config.red)
+  .addField("Insufficent Perm", perm);
+  message.channel.send(embed);
+}
+
+module.exports.userNotFound = (message) => {
+  let embed = new Discord.RichEmbed()
+  .setAuthor(message.author.username)
+  .setTitle("**USER NOT FOUND**")
+  .setColor(config.blue)
+  .addField("Error", "You did not specify a user!");
+  message.channel.send(embed);
+}
+
+module.exports.noQuestion = (message) => {
+    message.channel.send("I need a question buddy!")
+}
