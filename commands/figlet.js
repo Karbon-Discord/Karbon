b08accaf2bc9b7e50581bdcdb30c3b7afe9a2047
elementz <bfdifan21@gmail.com>
2018-08-25 20:32:26 -0400

    ASCII

---
+++ commands/figlet.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const figlet = require('figlet');
+module.exports.run = async (bot, message, args) => {
+    var sentence =  args.join(' ').slice(1)
+ 
+    figlet(`${sentence}`, (err, data) => {
+        if (err) {
+            console.log(err);
+            return;
+        }
+        message.channel.send(`````${data}`````)
+    });
+
+}
+
+module.exports.help = {
+  name: "figlet"
+}
