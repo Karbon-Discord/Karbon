a0319fe0a05222330537e9e140325968ad92facd
dotcomboom <38927017+dotcomboom@users.noreply.github.com>
2018-08-25 19:26:53 -0500

    Update ascii.js

--- commands/ascii.js
+++ commands/ascii.js
@@ -1,7 +1,6 @@
 const Discord = require('discord.js');
 const figlet = require('figlet');
 module.exports.run = async (bot, message, args) => {
-    args.slice()
     figlet(args.join(" "), (err, data) => {
         if (err) {
             console.log(err);
