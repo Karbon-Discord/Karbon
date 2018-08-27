ee33a17ab12e1a72219d7484de6d5f1bf23b90c0
elementz <bfdifan21@gmail.com>
2018-08-27 15:23:19 -0400

    oof

--- commands/stats.js
+++ commands/stats.js
@@ -1,5 +1,5 @@
 const Discord = require('discord.js');
-const errors = require('utils/errors.js');
+const errors = require('./utils/errors.js');
 const config = require('../botconfig.json');
 
 module.exports.run = async (bot, message, args) => {
