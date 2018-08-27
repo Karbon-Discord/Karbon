f948b3933f16f21e4a50f0c502e53b9d70f10dcc
Pizza Squeeze <32986973+nikillpop@users.noreply.github.com>
2018-08-27 12:45:55 -0400

    oof

--- commands/botsay.js
+++ commands/botsay.js
@@ -3,7 +3,7 @@ const errors = require('./utils/errors.js');
 
 module.exports.run = async (bot, message, args) => {
     var phrase = args.join(' ');
-    if(!phrase) return errors.correctUsage(message, "k:botsay [phrase]");
+    if(!phrase) return errors.correctUsage(message, "k:botsay [phrase] frick stop!!!");
     message.delete().catch();
     message.channel.send(phrase)
 
