000b0146bea68edd96b5353fd5a2093965a9b795
elementz <bfdifan21@gmail.com>
2018-08-27 18:24:36 -0400

    oof

--- commands/stats.js
+++ commands/stats.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
     let embed = new Discord.RichEmbed()
     .setColor(config.green)
     .setTitle("Karbon Discord Bot Statistics")
-    .setAuthor(message.author.id)
+    .setAuthor(message.author.username)
     .addField("Servers", bot.guilds.size)
     .addField("Members", bot.users.size)
     .addField("Channels", bot.channels.size);
