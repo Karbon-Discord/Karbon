d90acaa817c0b590607efb59937d7f08bb4b725e
elementz <bfdifan21@gmail.com>
2018-08-27 11:02:24 -0400

    Created botsay.js

--- commands/botsay.js
+++ commands/botsay.js
@@ -4,7 +4,7 @@ const errors = require('./utils/errors.js');
 module.exports.run = async (bot, message, args) => {
     var phrase = args.join(' ');
     if(!phrase) return errors.correctUsage(message, "k:botsay [phrase]");
-    message.delete();
+    message.delete().catch();
     message.channel.send(phrase)
 
 }
