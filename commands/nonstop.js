fc1a02326c5a38af4971bc716264f49ab83d232a
Pizza Squeeze <32986973+nikillpop@users.noreply.github.com>
2018-08-27 12:51:06 -0400

    Update nonstop.js

--- commands/nonstop.js
+++ commands/nonstop.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-   message.channel.send("k:botsay nonstop")
+   message.channel.send("k:botsay k:nonstop")
 };
 
 module.exports.help = {
