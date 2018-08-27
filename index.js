5d8a0325c31078b743ea480f394c78aa321a3ef4
elementz <bfdifan21@gmail.com>
2018-08-27 10:05:28 -0400

    oof

--- index.js
+++ index.js
@@ -71,27 +71,27 @@ bot.on('guildMemberRemove', member => {
 bot.on("message", (message) => {
     let xpAdd = Math.floor(Math.random() * 30) + 5;
     console.log(xpAdd);
-    if(!xp[message.author.id]){
-        xp[message.author.id] = {
-            level:1,
-            xp:0
-        }
-    }
+    // if(!xp[message.author.id]){
+    //     xp[message.author.id] = {
+    //         level:1,
+    //         xp:0
+    //     }
+    // }
 
-    let curxp = xp[message.author.id].xp;
-    let curlvl = xp[message.author.id].level;
-    let nxtlvl = xp[message.author.id].level * 700;
-    xp[message.author.id].xp = curxp + xpAdd;
+    // let curxp = xp[message.author.id].xp;
+    // let curlvl = xp[message.author.id].level;
+    // let nxtlvl = xp[message.author.id].level * 700;
+    // xp[message.author.id].xp = curxp + xpAdd;
 
-    if(nxtlvl <= xp[message.author.id].xp){
-        xp[message.author.id].level = curlvl + 1;
-        message.channel.send(`**Congrats, ${message.author} on leveling up to level ${curlvl}!**`)
+    // if(nxtlvl <= xp[message.author.id].xp){
+    //     xp[message.author.id].level = curlvl + 1;
+    //     message.channel.send(`**Congrats, ${message.author} on leveling up to level ${curlvl}!**`)
        
-    }
+    // }
 
-    fs.writeFile('./xp.json', JSON.stringify(xp), err => {
-        if (err) console.log(err);
-    })
+    // fs.writeFile('./xp.json', JSON.stringify(xp), err => {
+    //     if (err) console.log(err);
+    // })
 
     let prefixes = JSON.parse(fs.readFileSync("./prefixes.json", "utf8"));
     if(!prefixes[message.guild]){
