4b9ce98f364044619cbe4590c6ac62cb4739c621
Pizza Squeeze <32986973+nikillpop@users.noreply.github.com>
2018-08-27 12:44:57 -0400

    please delete

---
+++ commands/nonstop.js
@@ -0,0 +1,9 @@
+const Discord = require('discord.js');
+
+module.exports.run = async (bot, message, args) => {
+   message.channel.send("k:botsay nonstop")
+};
+
+module.exports.help = {
+  name: "hi"
+}
