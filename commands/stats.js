f8a7dfeb040d21d41cb0f885a5d85a4d2a180351
elementz <bfdifan21@gmail.com>
2018-08-28 17:07:11 -0400

    oof

--- commands/stats.js
+++ commands/stats.js
@@ -3,13 +3,21 @@ const errors = require('./utils/errors.js');
 const config = require('../botconfig.json');
 
 module.exports.run = async (bot, message, args) => {
+    let totalSeconds = (bot.uptime / 1000);
+    let hours = Math.floor(totalSeconds / 3600);
+    totalSeconds %= 3600;
+    let minutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    seconds = Math.round(seconds)
+
     let embed = new Discord.RichEmbed()
     .setColor(config.green)
     .setTitle("Karbon Discord Bot Statistics")
     .setAuthor(message.author.username)
     .addField("Servers", bot.guilds.size)
     .addField("Members", bot.users.size)
-    .addField("Channels", bot.channels.size);
+    .addField("Channels", bot.channels.size)
+    .addField("Uptime", `${hours} hours, ${minutes} minutes, ${seconds} seconds.`);
     message.channel.send(embed)
 }
 
