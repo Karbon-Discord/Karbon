4ae1002f2b408b5eb78a0bbc09bef7589f3b1e99
elementz <bfdifan21@gmail.com>
2018-08-29 15:57:43 -0400

    Height conversion

--- commands/category.js
+++ commands/category.js
@@ -2,6 +2,7 @@ const Discord = require('discord.js');
 const brain = require('brain.js');
 const data = require('../brain.json');
 const errors = require('./utils/errors.js')
+const network = new brain.recurrent.LSTM();
 
 module.exports.run = async (bot, message, args) => {
 
@@ -9,22 +10,18 @@ module.exports.run = async (bot, message, args) => {
  if(!obj) return errors.correctUsage(message, "--category [word | phrase]");
 
  try{
- message.channel.send("Thinking... NOTE: This will take a while for me to think."); 
+ const trainingData = data.map(item => ({
+   input: item.text,
+   output: item.category
+ }));
 
-//  const network = new brain.recurrent.LSTM();
+network.train(trainingData, {
+   iterations: 3000
+ });
 
-//  const trainingData = data.map(item => ({
-//    input: item.text,
-//    output: item.category
-//  }));
+ const output = network.run(`${obj}`);
 
-// network.train(trainingData, {
-//    iterations: 3000
-//  });
-
-//  const output = network.run(`${obj}`);
-
-//  message.reply(`**According to my virtual brain, the category for the phrase/word is: ${output}**`);
+ message.reply(`**According to my virtual brain, the category for the phrase/word is: ${output}**`);
 
  } catch(e){
      if(e) console.log(e)
