89c8f4526e8c36168b49e85a51dd57dd5b608328
elementz <bfdifan21@gmail.com>
2018-08-29 16:04:33 -0400

    Height conversion

--- commands/category.js
+++ commands/category.js
@@ -9,23 +9,20 @@ module.exports.run = async (bot, message, args) => {
  var obj = args.join(' ');
  if(!obj) return errors.correctUsage(message, "--category [word | phrase]");
 
- try{
- const trainingData = data.map(item => ({
-   input: item.text,
-   output: item.category
- }));
+ const network = new brain.recurrent.LSTM();
+
+const trainingData = data.map(item => ({
+  input: item.text,
+  output: item.category
+}));
 
 network.train(trainingData, {
-   iterations: 3000
- });
+  iterations: 3000
+});
 
- const output = network.run(`${obj}`);
+const res = network.run(obj);
 
- message.reply(`**According to my virtual brain, the category for the phrase/word is: ${output}**`);
-
- } catch(e){
-     if(e) console.log(e)
- }
+console.log(`Category: ${res}`);
 };
 
 module.exports.help = {
