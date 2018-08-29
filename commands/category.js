50eb2b22b4d2a38a332ecb1a0c0e07d45b6bd65c
elementz <bfdifan21@gmail.com>
2018-08-29 15:50:18 -0400

    Height conversion

--- commands/category.js
+++ commands/category.js
@@ -1,27 +1,38 @@
 const Discord = require('discord.js');
 const brain = require('brain.js');
 const data = require('../brain.json');
+const errors = require('./utils/errors.js')
 
 module.exports.run = async (bot, message, args) => {
-var obj = args.join(' ');
-message.channel.send("Thinking... NOTE: This will take a while for me to think."); 
 
-const network = new brain.recurrent.LSTM();
+ var obj = args.join(' ');
+ if(!obj) return errors.correctUsage(message, "--category [word | phrase]");
 
-const trainingData = data.map(item => ({
-  input: item.text,
-  output: item.category
-}));
+ try{
+ message.channel.send("Thinking... NOTE: This will take a while for me to think."); 
+
+ const network = new brain.recurrent.LSTM();
+
+ const trainingData = data.map(item => ({
+   input: item.text,
+   output: item.category
+ }));
 
 network.train(trainingData, {
-  iterations: 3000
-});
+   iterations: 3000
+ });
 
-const output = network.run(`${obj}`);
+ const output = network.run(`${obj}`);
 
-message.reply(`**According to my virtual brain, the category for the phrase/word is: ${output}**`);
-}
+ message.reply(`**According to my virtual brain, the category for the phrase/word is: ${output}**`);
+
+ } catch(e){
+     if(e) console.log(e)
+ }
+};
 
 module.exports.help = {
-    name: "category"
+  name: "category"
 }
+
+
