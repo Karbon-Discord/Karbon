e4c87cac3b94ad1491158067bb7b9d8d5f00a39f
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-29 18:29:18 -0400

    Update category.js

--- commands/category.js
+++ commands/category.js
@@ -1,28 +1,22 @@
 const Discord = require('discord.js');
 const brain = require('brain.js');
-const data = require('./data.json');
+//const data = require('./data.json');
 const errors = require('./utils/errors.js')
 module.exports.run = async (bot, message, args) => {
-    var user = args.join(' ');
-    if(!user) return errors.correctUsage(message, "--category [word or phrase]")
+   // var user = args.join(' ');
+    //if(!user) return errors.correctUsage(message, "--category [word or phrase]")
     
     var network = new brain.recurrent.LSTM();
-    const trainingData = data.map(item => ({
-    input: item.text,
-    output: item.category
-    }));
+    var net = new brain.recurrent.LSTM();
 
-    network.train(trainingData, {
-    iterations: 3000
-    });
-
-    const output = network.run('water').then(() => {
-        console.log(`Category: ${output}`);
-
-    }).catch(() => {
-        if(err) console.log(err)
-    })
-
+    net.train([
+      'doe, a deer, a female deer',
+      'ray, a drop of golden sun',
+      'me, a name I call myself',
+    ]);
+    
+    var output = net.run('doe');  // ', a deer, a female deer'
+    console.log(output);
 
 
 };
