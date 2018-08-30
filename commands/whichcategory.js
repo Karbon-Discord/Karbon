572690e17e86429f06182ca1cfcc724d8caa16f6
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 19:52:17 -0400

    Update whichcategory.js

--- commands/whichcategory.js
+++ commands/whichcategory.js
@@ -5,8 +5,10 @@ module.exports.run = async (bot, message, args) => {
 
   const phrase = args.join(' ');
     if(phrase) {
-        return message.channel.send("**THINKING** Please note this will take some time.")
-
+let msg = await message.channel.send(":warning: | **CONFIRMATION: Are you sure you want to run the which category script? This will make the bot go offline for 5-10 minutes.**");
+ const res = await message.channel.awaitMessages(response => {
+  response.content === "yes" || response.content === "no"
+  if(response.content === "yes"){
     console.log(phrase);
 
   const network = new brain.recurrent.LSTM();
@@ -26,11 +28,13 @@ module.exports.run = async (bot, message, args) => {
                  { input: "3", output: "Number" },
 
     ])
-
+  }
     const output = network.run(`${phrase}`);
-    message.reply(`Prob: ${output}`);
-    if(output !== "Drink" || output !== "Place" || output !== "Number") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
+    message.reply(`Category: ${output}`);
     }
+}
+else if(response.content === "no") return message.reply(":x: | ** Will not run the script. **")
+
 }
 module.exports.help = {
   name: "whichcategory"
