14fa561ab5f5fab214e8f8d6422bf56f192033ca
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 16:20:04 -0400

    Update drink.js

--- commands/drink.js
+++ commands/drink.js
@@ -7,14 +7,9 @@ module.exports.run = async (bot, message, args) => {
     network.train([
         { input: "Water", output: "Drink" },
         { input: "Juice", output: "Drink" },
-        { input: "Lemonade", output: "Drink" },
-        { input: "Coke", output: "Soda" },
-        { input: "Sprite", output: "Soda" },
-        { input: "Fanta", output: "Soda" },
-        { input: "Pepsi", output: "Soda" },
-        { input: "Mountain Dew", output: "Soda" }
+        { input: "Lemonade", output: "Drink" }
     ])
-    const output = network.run(`water`);
+    const output = network.run("coke");
     message.reply(`Prob: ${output}`);
     if(output === "") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
 
