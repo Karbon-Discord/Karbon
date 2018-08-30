4de0b3968cfc870f94aacb743936e074b87a01ee
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 19:35:39 -0400

    Update whichcategory.js

--- commands/whichcategory.js
+++ commands/whichcategory.js
@@ -2,9 +2,11 @@ const Discord = require('discord.js');
 const brain = require('brain.js');
 
 module.exports.run = async (bot, message, args) => {
-    message.channel.send("**THINKING** Please note this will take some time.")
 
   const phrase = args.join(' ');
+    if(phrase) {
+        return message.channel.send("**THINKING** Please note this will take some time.")
+
     console.log(phrase);
 
   const network = new brain.recurrent.LSTM();
@@ -28,7 +30,7 @@ module.exports.run = async (bot, message, args) => {
     const output = network.run(`${phrase}`);
     message.reply(`Prob: ${output}`);
     if(output !== "Drink" || output !== "Place" || output !== "Number") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
-
+    }
 }
 module.exports.help = {
   name: "whichcategory"
