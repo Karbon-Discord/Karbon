5e9d7b89ddbd02805843eaa5c683d0344f36e6c9
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 18:14:41 -0400

    Update whichcategory.js

--- commands/whichcategory.js
+++ commands/whichcategory.js
@@ -2,25 +2,40 @@ const Discord = require('discord.js');
 const brain = require('brain.js');
 
 module.exports.run = async (bot, message, args) => {
+    message.channel.send("**THINKING** Please note this will take some time.")
 
   const phrase = args.join(' ');
     console.log(phrase);
 
-    message.channel.send("**THINKING** Please note this will take up to 2-3 minutes.")
   const network = new brain.recurrent.LSTM();
-    network.train([
+ network.train([
         { input: "Water", output: "Drink" },
         { input: "Juice", output: "Drink" },
         { input: "Lemonade", output: "Drink" },
+         { input: "Coke", output: "Drink" },
+        { input: "Sprite", output: "Drink" },
+         { input: "Fanta", output: "Drink" },
         { input: "Los Angeles", output: "Place" },
         { input: "San Francisco", output: "Place" },
            { input: "Portland", output: "Place" },
+           { input: "Toronto", output: "Place" },
+           { input: "Boston", output: "Place" },
+           { input: "New Delhi", output: "Place" },
+           { input: "Lahore", output: "Place" },
+           { input: "Seoul", output: "Place" },
          { input: "0", output: "Number" },
            { input: "1", output: "Number" },
                  { input: "2", output: "Number" },
                  { input: "3", output: "Number" },
+                 { input: "4", output: "Number" },
+                 { input: "5", output: "Number" },
+                 { input: "6", output: "Number" },
+                 { input: "7", output: "Number" },
+                 { input: "8", output: "Number" },
+                 { input: "9", output: "Number" },
 
     ])
+
     const output = network.run(`${phrase}`);
     message.reply(`Prob: ${output}`);
     if(output !== "Drink" || output !== "Place" || output !== "Number") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
