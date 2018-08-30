cd5bc277ce604b3160d59cc992f441ea7e8bb1a7
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 16:29:24 -0400

    Update drink.js

--- commands/drink.js
+++ commands/drink.js
@@ -12,7 +12,7 @@ module.exports.run = async (bot, message, args) => {
   console.log(phrase);
     const output = network.run(`${phrase}`);
     message.reply(`Prob: ${output}`);
-    if(output === "") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
+    if(output !== "Drink") return message.reply("**Looks like this word doesn't come under a drink or a soda!**")
 
 }
 module.exports.help = {
