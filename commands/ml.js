898a9c7afdb3745f8ce6350a689b0507b2e43819
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 13:37:43 -0400

    Update ml.js

--- commands/ml.js
+++ commands/ml.js
@@ -2,14 +2,6 @@ const Discord = require('discord.js');
 const brain = require('brain.js');
 
 module.exports.run = async (bot, message, args) => {
-const network = new brain.recurrent.LSTM();
-    network.train([
-        { input: "Water", output: "Drink" },
-        { input: "Juice", output: "Drink" },
-        { input: "Lemonade", output: "Drink" }
-    ])
-    const output = network.run("Mango Juice");
-    console.log(`Prob: ${output}`);
 
 }
 module.exports.help = {
