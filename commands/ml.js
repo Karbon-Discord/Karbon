7f12566146c76f4cc0b2d3643007fed8c789c16a
elementz <33937944+BigBallerBrand2@users.noreply.github.com>
2018-08-30 13:27:23 -0400

    Update ml.js

--- commands/ml.js
+++ commands/ml.js
@@ -2,6 +2,14 @@ const Discord = require('discord.js');
 const brain = require('brain.js');
 
 module.exports.run = async (bot, message, args) => {
+const network = new brain.NeuralNetwork();
+    network.train([
+        { input: [0, 0, 0], output: [1] },
+        { input: [0, 1, 1], output: [1] },
+        { input: [1, 0, 1], output: [1] }
+    ])
+    const output = network.run([1, 0, 1]);
+    console.log(`Prob: ${output}`);
 
 }
 module.exports.help = {
