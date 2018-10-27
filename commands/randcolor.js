c48d7572c0faec1ebf685f4513958ca8e8cffaa5
elementz <33937944+elementzprojects@users.noreply.github.com>
2018-10-26 20:58:31 -0400

    Update randcolor.js

--- commands/randcolor.js
+++ commands/randcolor.js
@@ -1,7 +1,11 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
+  var color = Math.floor(Math.random() * 0xFFFFFF).toString(16);
   var embed = new Discord.RichEmbed()
-    .setDescription(`Random Color is: #${Math.floor(Math.random() * 0xFFFFFF).toString(16)}`);
+    .setTitle("Random Color")
+    .setAuthor(message.author.tag)
+    .setColor(`${color}`)
+    .setDescription(`Random Color is: #${color)}`);
   message.channel.send(embed)
 };
 module.exports.help = {
