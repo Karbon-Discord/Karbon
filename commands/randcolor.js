07f680d6e822b92f66edafe60d1a365bdcd043a0
Gopal Othayoth <Gopal.Othayoth@hotmail.com>
2018-10-27 17:15:03 -0500

    Removed unnecessary variable and template string
    Used new Discord.RichEmbed() directly in the send function instead of an embed variable
    Replaced `${color}` with color, as color is already a string

--- commands/randcolor.js
+++ commands/randcolor.js
@@ -1,12 +1,12 @@
 const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
-  var color = Math.floor(Math.random() * 0xFFFFFF).toString(16);
-  var embed = new Discord.RichEmbed()
+  let color = Math.floor(Math.random() * 0xFFFFFF).toString(16);
+  message.channel.send(new Discord.RichEmbed()
     .setTitle("Random Color")
     .setAuthor(message.author.tag)
-    .setColor(`${color}`)
+    .setColor({color})
     .setDescription(`Random Color is: #${color}`);
-  message.channel.send(embed)
+  );
 };
 module.exports.help = {
   name: "randcolor"
