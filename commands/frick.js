ad9303e2ea66dfcd10c9e432259f9273eeb8dc0e
CerebralDatabank <Gopal.Othayoth@hotmail.com>
2018-12-05 08:33:05 -0600

    Added a foot emoji to the end of the "frick" command
    Foot emoji codepoint: U+1F9B6

--- commands/frick.js
+++ commands/frick.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-   message.channel.send("my T O E!")
+   message.channel.send("my T O E! \u{1F9B6}")
 };
 
 module.exports.help = {
