311e5e71ebde7f511795226b6e14a3ed559ef7a6
Gopal Othayoth <24465111+CerebralDatabank@users.noreply.github.com>
2019-03-19 13:24:17 -0500

    Testing things Just for debugging, will be fixed

--- commands/discordid.js
+++ commands/discordid.js
@@ -3,6 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     if (args[0]) {
       message.channel.send(args[0]);
+      message.channel.send(message.mentions.members.first());
     }
     else {
       message.react("🆔");
