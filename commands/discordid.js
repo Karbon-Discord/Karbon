638357531ac69feece55b2fe20a22c6edef4c6bb
Gopal Othayoth <24465111+CerebralDatabank@users.noreply.github.com>
2019-03-19 13:19:36 -0500

    Improving Discord ID
    Adding functionality (find Discord ID of other users)

--- commands/discordid.js
+++ commands/discordid.js
@@ -1,9 +1,13 @@
 const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-    message.react("🆔");
-    message.reply(`**Your Discord ID is: ${message.author.id}**`);
-    message.reply("debug: " + args[0]);
+    if (args[0]) {
+      message.channel.send(args[0]);
+    }
+    else {
+      message.react("🆔");
+      message.reply(`**Your Discord ID is: ${message.author.id}**`);
+    }
 }
 
 module.exports.help = {
