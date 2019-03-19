7ed3b032209ab3ff4cd017ec2f9828be2581ba69
Gopal Othayoth <24465111+CerebralDatabank@users.noreply.github.com>
2019-03-19 13:34:11 -0500

    Consistent behavior ensured :) Made the --discordid command be bolded just like the command by itself.

--- commands/discordid.js
+++ commands/discordid.js
@@ -3,7 +3,7 @@ const Discord = require('discord.js');
 module.exports.run = async (bot, message, args) => {
     message.react("🆔");
     if (args[0]) {
-      message.channel.send(`Discord ID of that user: ${message.mentions.members.first().id}`);
+      message.channel.send(`**Discord ID of that user: ${message.mentions.members.first().id}**`);
     }
     else {
       message.reply(`**Your Discord ID is: ${message.author.id}**`);
