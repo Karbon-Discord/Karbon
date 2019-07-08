c950c56f57099e84e91d41cadb4bf0dca95fac71
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-07-08 19:19:12 -0400

    Update temppred.js

--- commands/temppred.js
+++ commands/temppred.js
@@ -7,7 +7,7 @@ module.exports.run = async (bot, message, args) => {
     else{
          const model = await tf.loadLayersModel("https://elementzprojects.github.io/Work/Neural%20Networks/Temperature%20Predictor/model.json")
          let pred = model.predict(tf.tensor2d([parseInt(minTemp)], [1, 1]))
-         message.channel.send(`The Predicted Maximum Temperature is: ${pred.dataSync()[0]}°F`)
+         message.channel.send(`The Predicted Maximum Temperature is: ${parseInt(pred.dataSync()[0])}°F`)
     }
 }
 
