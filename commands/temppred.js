ad4f2128f417d3d326edbd4e0965463057726ba0
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-07-08 19:00:08 -0400

    Update temppred.js

--- commands/temppred.js
+++ commands/temppred.js
@@ -1,12 +1,12 @@
 const Discord = require('discord.js');
-
+const model = require('https://github.com/Karbon-Discord/Karbon/blob/master/commands/tfmodels/temperaturemodel/model.json')
 const errors = require('./utils/errors.js')
 const tf = require('@tensorflow/tfjs')
 module.exports.run = async (bot, message, args) => {
     let minTemp = args[0]
     if(!minTemp) return errors.correctUsage(message, "--temppred [minimum temperature]");
     else{
-         const model = await tf.loadLayersModel("https://github.com/Karbon-Discord/Karbon/blob/master/commands/tfmodels/temperaturemodel/model.json")
+         const model = await tf.loadLayersModel(model)
          let pred = model.predict(tf.tensor2d([parseInt(minTemp)], [1, 1]))
          message.channel.send(`The Predicted Maximum Temperature is: ${pred.dataSync()[0]}°F`)
     }
