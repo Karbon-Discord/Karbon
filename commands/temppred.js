10548b0e2f3775434eaf6400bc01f2c7efc911df
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-07-08 19:18:05 -0400

    Update temppred.js

--- commands/temppred.js
+++ commands/temppred.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-const modele = require('./tfmodels/temperaturemodel/model.json')
 const errors = require('./utils/errors.js')
 const tf = require('@tensorflow/tfjs')
 module.exports.run = async (bot, message, args) => {
     let minTemp = args[0]
     if(!minTemp) return errors.correctUsage(message, "--temppred [minimum temperature]");
     else{
-         const model = await tf.loadLayersModel(modele)
+         const model = await tf.loadLayersModel("https://elementzprojects.github.io/Work/Neural%20Networks/Temperature%20Predictor/model.json")
          let pred = model.predict(tf.tensor2d([parseInt(minTemp)], [1, 1]))
          message.channel.send(`The Predicted Maximum Temperature is: ${pred.dataSync()[0]}°F`)
     }
