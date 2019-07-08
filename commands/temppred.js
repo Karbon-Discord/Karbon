6e4c4b59bec9d749185c62f07dc8336bb2eb92f6
elementzprojects <33937944+elementzprojects@users.noreply.github.com>
2019-07-08 18:32:41 -0400

    Create temppred.js
    New command! A TensorFlow Neural Network that tackles the problem whether or not there is a linear relationship between the minimum temperature and the maximum temperature. You give the bot a min temperature, and the bot will try to predict the maximum temperature.

---
+++ commands/temppred.js
@@ -0,0 +1,18 @@
+const Discord = require('discord.js');
+const model = require('./tfmodels/temperaturemodel/model.json');
+const errors = require('./utils/errors.js')
+const tf = require('@tensorflow/tfjs')
+module.exports.run = (bot, message, args) => {
+    let minTemp = args[0]
+    if(!minTemp) return errors.correctUsage(message, "--temppred [minimum temperature]");
+    if(typeof parseInt(minTemp) !== "int") return message.channel.send("Please provide an integer.")
+    else{
+         let pred = model.predict(tf.tensor2d([parseInt(minTemp)], [1, 1]))
+         message.channel.send(`The Predicted Maximum Temperature is: ${pred.dataSync()[0]}°F`)
+    }
+}
+
+
+module.exports.help = {
+    name: "temppred"
+}
